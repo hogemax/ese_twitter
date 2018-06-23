c662b47b7dc9d5810f92b1fcc37eebcdbbcc92af
hogemax <jomokoo@gmail.com>
2018-06-24 02:42:56 +0900

    Fix js files coding

--- app/assets/javascripts/post-button-decorator.js
+++ app/assets/javascripts/post-button-decorator.js
@@ -1,24 +1,23 @@
-$(document).on('turbolinks:load', function() {
-//$(function(){
+$(document).on('turbolinks:load', function() { //$(function(){
+  //新規投稿ボタン（POST）
+  var newPostButton = $('div.container form.new_micropost').find('input[name="commit"]');
+
+  //新規投稿入力欄に文字列が入力されていればPOSTボタンの表示
   $('aside.col-md-4 textarea#micropost_content').on('input click keypress blur', function(){
-  //$('textarea#micropost_content').on('input click keypress blur', function(){
-    var contents = $(this).val();
-    var postButton = $('form.new_micropost').find('input[name="commit"]');
-
-    if(contents === "") {
-      //console.log("空");
-      postButton.removeClass( "btnAttractAttentionOne gradientStyleOne" );
-      //postButton.val("post")
-    } else {
-      //console.log("入力値あり");
-      postButton.addClass( "btnAttractAttentionOne gradientStyleOne" );
-      //postButton.val("post")
-    }
+    ($(this).val() === "") ? postButtonDecoRemove( newPostButton ): postButtonDecoAdd( newPostButton );
   });
 
-  //モーダル開いたとき
-  $('div#repostModal').on('show.bs.modal', function (event) {
-      //新規投稿の内容が入力してあれば一旦空にする
-      $('form.new_micropost').find('input[name="commit"]').removeClass( "btnAttractAttentionOne gradientStyleOne" );
-  });
+  //モーダル開いたとき（新規投稿の内容が入力してあれば一旦空にする）
+  //$('div#repostModal').on('show.bs.modal', function (event) {
+  //    postButtonDecoRemove( newPostButton ); //newPostButton.removeClass( "btnAttractAttentionOne gradientStyleOne" );
+  //});
 });
+
+//---- Global Function ----//
+
+function postButtonDecoRemove( targetButton ) {
+  targetButton.removeClass( "btnAttractAttentionOne gradientStyleOne" );
+}
+function postButtonDecoAdd( targetButton ) {
+  targetButton.addClass( "btnAttractAttentionOne gradientStyleOne" );
+}
--- app/assets/javascripts/repost.js
+++ app/assets/javascripts/repost.js
@@ -1,9 +1,8 @@
-$(document).on('turbolinks:load', function() {
-//$(function(){
-
+$(document).on('turbolinks:load', function() { //$(function(){
   //モーダルを開いているかのフラグ
   var modalFlag = false;
 
+  //モーダルが開いた時（引用投稿ボタンを押した時）の処理
   $('div#repostModal').on('show.bs.modal', function (event) {
     //投稿内容の初期化
     revertPostlook();
@@ -12,15 +11,12 @@ $(document).on('turbolinks:load', function() {
 
     var button = $(event.relatedTarget) //モーダルを呼び出すときに使われたボタンを取得
     var post_itself = button.parent().parent()
-    var post_id = post_itself.attr('id')
-    var post_content = post_itself.find('span.content').text()
+    var post_id = post_itself.attr('id') //console.log(post_id)
+    var post_content = post_itself.find('span.content').text() //console.log(post_content)
     var post_image
     //画像の取得
     (post_itself.find('div').find('img')) ? post_image = post_itself.find('div').find('img').attr('src'): post_image = "";
     var recipient = button.data('whatever') //data-whatever の値を取得
-
-    //console.log(post_id)
-    //console.log(post_content)
 
     var modal = $(this)  //モーダルを取得
     modal.find('.modal-title').text('Repost of ' + recipient) //モーダルのタイトルに値を表示
@@ -32,15 +28,12 @@ $(document).on('turbolinks:load', function() {
     modal_source_div.find('#repost_source_image').attr('src', src_image_url);
   });
 
-  //モーダルでのpostボタン装飾判定
+  //引用投稿ボタン（POST）
+  var repostButton = $('div.modal-content form.new_micropost').find('input[name="commit"]');
+
+  //モーダルでのpostボタン装飾判定（入力欄に文字列が入力されていればPOSTボタンの表示）
   $('div.modal-dialog textarea#micropost_content').on('input click keypress blur', function(){
-      var repostContents = $(this).val();
-      var repostButton = $('form.new_micropost').find('input[name="commit"]');
-      if(repostContents === "") {
-        repostButton.removeClass( "btnAttractAttentionOne gradientStyleOne" );
-      } else {
-        repostButton.addClass( "btnAttractAttentionOne gradientStyleOne" );
-      }
+    ($(this).val() === "") ? postButtonDecoRemove( repostButton ): postButtonDecoAdd( repostButton );
   });
 
   //「x」または「close」ボタンを押したとき
@@ -48,18 +41,20 @@ $(document).on('turbolinks:load', function() {
   $('div.modal-footer button').on('click', revertPostlook);
   //モーダルを開いた状態で「esc」キーを押したとき
   $( "body" ).keydown(function( event ) {
-    if ( event.which == 27 && modalFlag == true ) {
-      revertPostlook();
-    }
+    if ( event.which == 27 && modalFlag == true ) revertPostlook();
   });
+
+  //引用投稿内容（モーダル内）
+  var modalPostContent = $('div.modal-content textarea#micropost_content');
+  //var newPostContent = $('div.container textarea#micropost_content');
 
   //内容を空にしてpostボタンの装飾クラスも除去
   function revertPostlook () {
-    $('textarea#micropost_content').val("");
-    $('form.new_micropost').find('input[name="commit"]').removeClass( "btnAttractAttentionOne gradientStyleOne" );
+    modalPostContent.val(""); //$('div.modal-dialog textarea#micropost_content').val("");
+    postButtonDecoRemove( repostButton ); //repostButton.removeClass( "btnAttractAttentionOne gradientStyleOne" );
+
     //モーダル無効化
     modalFlag = false;
-    //新規投稿ボタンの装飾も戻す
-    $('aside.col-md-4 textarea#micropost_content').removeClass( "btnAttractAttentionOne gradientStyleOne" );
+    //postButtonDecoRemove( newPostContent ); //新規投稿ボタンの装飾も除去
   }
 });
