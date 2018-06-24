a7c69a0792119ad8776ff41fe1ffabc9178f4bdc
hogemax <jomokoo@gmail.com>
2018-06-25 02:21:15 +0900

    Fix Image upload function for Env-Production

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,18 +1,14 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-#  if Rails.env.production?
-#    include Cloudinary::CarrierWave
-#  else
-#    include CarrierWave::RMagick
-#  end
-
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+    process :convert => 'png'
+    process :tags => ['image']
+  else
+    include CarrierWave::RMagick
+    storage :file
+    # storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -52,6 +48,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
   #ファイルサイズ制限
   def size_range
     1..9.megabytes
