8c2cdb0ea48a6f804ac405685d9ef4cf686ce84a
hogemax <jomokoo@gmail.com>
2018-07-04 01:09:42 +0900

    Add Setting of action_mailer (for Production)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  #問い合わせ機能用の設定
+  s_mail = ENV['SMTP_MAIL']
+  s_pass = ENV['SMTP_PASS']
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :domain => 'gmail.com',
+    :port => 587,
+    :user_name => s_mail,
+    :password => s_pass,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
