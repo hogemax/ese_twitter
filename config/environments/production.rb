eff68ce220b1bd3e2c90bc33b5dc00e1297849e1
hogemax <jomokoo@gmail.com>
2018-07-04 01:17:51 +0900

    Fix Option of action_mailer (for Production)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
   s_pass = ENV['SMTP_PASS']
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
