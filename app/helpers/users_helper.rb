c922b546f9556106a94c39b032916cae9b85d2ad
hogemax <jomokoo@gmail.com>
2018-07-14 15:41:05 +0900

    Change Hash format for Gravatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,11 @@ module UsersHelper
 
   # 該当ユーザーのメールアドレスに対応するGravatarの画像URLを返す
   def gravatar_for(user, options = { size: 50 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::SHA2::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    #rbh_size = "#{options[:size]}x#{options[:size]}"
+    #gravatar_url = "https://robohash.org/#{gravatar_id}.jpg?size=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
